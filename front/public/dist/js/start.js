b41e0f07d5068fe1228b62732564c641dd8c6d0d
junpei-chan <jumpei24s1102270@gmail.com>
2025-08-27 22:12:18 +0900

    componentsフォルダでコンパイルできるよう設定

--- front/public/dist/js/start.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var _a;
-(_a = document.getElementById("btn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
-    alert("注文を開始しますか？"); // テスト
-});
--- front/scripts/start.ts
+++
@@ -1,3 +0,0 @@
-document.getElementById("btn")?.addEventListener("click", () => {
-  alert("注文を開始しますか？"); // テスト
-});
