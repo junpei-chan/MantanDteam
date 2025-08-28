dfd6953c26c9819f4cba109888e12ecf469c500d
junpei-chan <jumpei24s1102270@gmail.com>
2025-08-28 21:58:38 +0900

    SCSS → CSSへのコンパイルをフォルダ全体から監視して実行できるようにするコマンドを実装

---
+++ front/scripts/watch-all.js
@@ -0,0 +1,44 @@
+// scripts/watch-all.js
+// 複数のtsc --watchを並列起動するスクリプト
+const { spawn } = require('child_process');
+
+const fs = require('fs');
+const path = require('path');
+
+// front配下の全てのsrc/tsconfig.jsonを再帰的に探索
+function findTsconfigs(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(findTsconfigs(filePath));
+    } else if (file === 'tsconfig.json' && filePath.includes('/src/')) {
+      // src配下のtsconfig.jsonのみ対象
+      results.push(path.relative(process.cwd(), filePath));
+    }
+  });
+  return results;
+}
+
+const rootDir = process.cwd();
+const watchTargets = findTsconfigs(rootDir);
+
+if (watchTargets.length === 0) {
+  console.log('No src/tsconfig.json found.');
+  process.exit(0);
+}
+
+console.log('Watching the following tsconfig.json files:');
+watchTargets.forEach(t => console.log('  ' + t));
+
+watchTargets.forEach(tsconfig => {
+  const proc = spawn('npx', ['tsc', '--project', tsconfig, '--watch'], {
+    stdio: 'inherit',
+    shell: true
+  });
+  proc.on('close', code => {
+    console.log(`[watch] ${tsconfig} exited with code ${code}`);
+  });
+});
