4af681e168008d0ed9e2992da74142e35a1e3012
junpei-chan <jumpei24s1102270@gmail.com>
2025-08-29 12:29:46 +0900

    app/templates直下のcssファイルもdistに出力されるよう変更

--- front/scripts/watch-all-sass.sh
+++ front/scripts/watch-all-sass.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 for src in app/templates/style.scss app/templates/*/src components/features/*/*/src; do
+  if [ "$src" = "app/templates/style.scss" ]; then
+    mkdir -p app/templates/dist
+    npx sass --watch "$src":"app/templates/dist/style.css" &
+    continue
+  fi
   if [ -f "$src" ] && [[ "$src" == *.scss ]]; then
     dist="${src%.scss}.css"
     npx sass --watch "$src":"${dist}" &
