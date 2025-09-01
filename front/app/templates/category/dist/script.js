1420aab7a29bdd3bb78021d8cbccb6f0289ca1fa
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-01 12:20:46 +0900

    takeout-buttonコンポーネントのUI作成

--- front/app/templates/category/dist/script.js
+++ front/app/templates/category/dist/script.js
@@ -1,1 +1,2 @@
-"use strict";
+import { FetchComponent } from "../../../utils/dist/fetch-component.js";
+FetchComponent("CallBtnContainer", "/components/shared/button/takeout/src/index.html", "TakeoutBtn");
---
+++ front/app/templates/category/src/script.ts
@@ -0,0 +1,7 @@
+import { FetchComponent } from "../../../utils/dist/fetch-component.js";
+
+FetchComponent(
+  "CallBtnContainer", 
+  "/components/shared/button/takeout/src/index.html", 
+  "TakeoutBtn"
+);
