be2cab0fa95f5279fe5f015f1d083ff736a53f3e
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-02 13:29:01 +0900

    order-basketのUI作成

---
+++ front/app/templates/menu/src/script.ts
@@ -0,0 +1,31 @@
+import { FetchComponent } from "../../../utils/dist/fetch-component.js";
+
+FetchComponent(
+  "TakeoutBtnContainer", 
+  "/components/shared/button/takeout/src/index.html", 
+  "TakeoutBtn"
+);
+
+FetchComponent(
+  "AccountingBtnContainer", 
+  "/components/shared/button/accounting/src/index.html", 
+  "AccountingBtn"
+);
+
+FetchComponent(
+  "CallBtnContainer", 
+  "/components/shared/button/call/src/index.html", 
+  "CallBtn"
+);
+
+FetchComponent(
+  "OrderBtnContainer", 
+  "/components/shared/button/order/src/index.html", 
+  "OrderBtn"
+);
+
+FetchComponent(
+  "OrderHistoryBtnContainer", 
+  "/components/shared/button/order-history/src/index.html", 
+  "OrderHistoryBtn"
+);
