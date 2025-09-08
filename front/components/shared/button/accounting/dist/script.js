b0ad24bddc293af9235de6f861ba7ebfd81e1581
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-08 13:12:38 +0900

    accounting-modalのUI作成

--- front/components/shared/button/accounting/dist/script.js
+++ front/components/shared/button/accounting/dist/script.js
@@ -3,7 +3,6 @@ const accountingModal = document.getElementById("AccountingModalContainer");
 const btn = document.getElementById("AccountingBtn");
 if (btn && accountingModal) {
     btn.addEventListener("click", () => {
-        console.log("test");
         accountingModal.showModal();
     });
 }
--- front/components/shared/button/accounting/src/script.ts
+++ front/components/shared/button/accounting/src/script.ts
@@ -3,7 +3,6 @@ const btn = document.getElementById("AccountingBtn") as HTMLElement | null;
 
 if (btn && accountingModal) {
   btn.addEventListener("click", () => {
-    console.log("test")
     accountingModal.showModal();
   });
 }
