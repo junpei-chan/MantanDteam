0585f5879dbb3169d05ed2c08bc0ba0e2d1a1478
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-08 15:38:33 +0900

    call-modalのコンテンツ切り替えを実装

--- front/components/features/menu/call-modal/dist/script.js
+++ front/components/features/menu/call-modal/dist/script.js
@@ -1,8 +1,31 @@
 "use strict";
 const callCancelBtn = document.getElementById("cancelBtn");
 const callDialog = document.getElementById("CallModalContainer");
+const callBtns = document.querySelectorAll(".call-buttons button");
+const callContent = document.querySelector(".call");
+const standbyContent = document.querySelector(".standby");
 if (callCancelBtn && callDialog) {
     callCancelBtn.addEventListener("click", () => {
         callDialog.close();
+    });
+}
+if (callDialog && callContent && standbyContent) {
+    const observer = new MutationObserver(() => {
+        if (callDialog.open) {
+            callContent.style.removeProperty('display');
+            standbyContent.style.display = "none";
+        }
+    });
+    observer.observe(callDialog, { attributes: true, attributeFilter: ["open"] });
+}
+if (callContent && standbyContent) {
+    callBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            callContent.style.display = "none";
+            standbyContent.style.display = "flex";
+            setTimeout(() => {
+                callDialog === null || callDialog === void 0 ? void 0 : callDialog.close();
+            }, 1500);
+        });
     });
 }
--- front/components/features/menu/call-modal/src/script.ts
+++ front/components/features/menu/call-modal/src/script.ts
@@ -1,8 +1,34 @@
 const callCancelBtn = document.getElementById("cancelBtn") as HTMLElement | null;
 const callDialog = document.getElementById("CallModalContainer") as HTMLDialogElement | null;
+const callBtns = document.querySelectorAll(".call-buttons button") as NodeListOf<HTMLButtonElement>;
+const callContent = document.querySelector(".call") as HTMLElement | null;
+const standbyContent = document.querySelector(".standby") as HTMLElement | null;
 
 if (callCancelBtn && callDialog) {
   callCancelBtn.addEventListener("click", () => {
     callDialog.close();
+  });
+}
+
+if (callDialog && callContent && standbyContent) {
+  const observer = new MutationObserver(() => {
+    if (callDialog.open) {
+      callContent.style.removeProperty('display');
+      standbyContent.style.display = "none";
+    }
+  });
+  observer.observe(callDialog, { attributes: true, attributeFilter: ["open"] });
+}
+
+if (callContent && standbyContent) {
+  callBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      callContent.style.display = "none";
+      standbyContent.style.display = "flex";
+
+      setTimeout(() => {
+        callDialog?.close();
+      }, 1500);
+    });
   });
 }
