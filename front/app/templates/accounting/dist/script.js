ae408bac6522ed9c71c3938a7fce446634a35988
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-10 17:32:14 +0900

    accounting-pageのcomplatedを作成

--- front/app/templates/accounting/dist/script.js
+++ front/app/templates/accounting/dist/script.js
@@ -1,1 +1,10 @@
-export {};
+"use strict";
+const contents = document.querySelector(".main-contents");
+const completed = document.querySelector(".completed");
+const btn = document.querySelector(".accountingBtn");
+if (contents && completed && btn) {
+    btn.addEventListener("click", () => {
+        contents.style.display = "none";
+        completed.style.display = "block";
+    });
+}
--- front/app/templates/accounting/src/script.ts
+++ front/app/templates/accounting/src/script.ts
@@ -1,1 +1,10 @@
-import { FetchComponent } from "../../../utils/dist/fetch-component.js";
+const contents = document.querySelector(".main-contents") as HTMLElement | null;
+const completed = document.querySelector(".completed") as HTMLElement | null;
+const btn = document.querySelector(".accountingBtn") as HTMLElement | null;
+
+if (contents && completed && btn) {
+  btn.addEventListener("click", () => {
+    contents.style.display = "none";
+    completed.style.display = "block";
+  });
+}
